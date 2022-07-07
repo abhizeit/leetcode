1995f93ada3f1f667add419dbd4912094c0c862d
abhizeit <abhijeitsarraf@gmail.com>
2022-07-08 00:21:22 +0530

    solve merge sorted using two pointer

--- arrays/MergeSorted.java
+++ arrays/MergeSorted.java
@@ -3,31 +3,24 @@ package leetcode.arrays;
 import java.util.Arrays;
 
 public class MergeSorted {
-    static int[] merge(int[] nums1, int m, int[] nums2,int n) {
-        for(int i=0;i<n ;i++){
-            for(int j=0;j<m;j++){
-                if(nums2[i]<=nums1[j]){
-                    int temp=nums1[j];
-                    nums1[j]=nums2[i];
-                    for(int k =j+1;k<n+1;k++){
-                        int temp2 = nums1[k];
-                        nums1[k]= temp;
-                        temp=temp2;
-                    }
-                }
-
+    static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int pointer1 = m - 1, pointer2 = n - 1, index = m + n - 1;
+        while (pointer2 >= 0) {
+            if (pointer1 >= 0 && nums1[pointer1] > nums2[pointer2]) {
+                nums1[index--] = nums1[pointer1--];
+            } else {
+                nums1[index--] = nums2[pointer2--];
             }
         }
         return nums1;
     }
 
     public static void main(String[] args) {
-        int[]nums1 ={1,6,9,0,0,0,0};
-        int[]nums2 = {2,3,4,5};
-        int m=7;
-        int n=4;
-        int [] ans = merge(nums1,m,nums2 ,n);
+        int[] nums1 = {2, 0};
+        int[] nums2 = {1};
+        int m = 1;
+        int n = 1;
+        int[] ans = merge(nums1, m, nums2, n);
         System.out.println(Arrays.toString(ans));
-        System.out.println(nums1[7]);
     }
 }
