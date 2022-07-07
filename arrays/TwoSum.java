15c75e5b336eda062419951645c994f03e178580
abhizeit <abhijeitsarraf@gmail.com>
2022-07-07 10:42:40 +0530

    implemented two pointer approach

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -4,22 +4,37 @@ import java.util.Arrays;
 
 public class TwoSum {
 
-    public static  int[] twosum(int[]arr, int target){
-        int[] result = new int[2];
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
-                    result[0]=i;
-                    result [1]=j;
-                    return result;
-                }
+    public static int[] twosum(int[] arr, int target) {
+//        int[] result = new int[2];
+//        for(int i=0;i<arr.length-1;i++){
+//            for(int j=i+1;j<arr.length;j++){
+//                if(arr[i]+arr[j]==target){
+//                    result[0]=i;
+//                    result [1]=j;
+//                    return result;
+//                }
+//            }
+        //USING TWO POINTER APPROACH
+        int[] result = {-1, -1};
+        int i = 0;
+        int j = arr.length - 1;
+        while (i != j) {
+            if (arr[i] + arr[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            } else if (arr[i] + arr[j] > target) {
+                j--;
+            } else {
+                i++;
             }
-        }return  result;
+        }
+        return result;
     }
 
 
     public static void main(String[] args) {
-        int[] nums={2,6, 4, 5};
+        int[] nums = {2, 6, 4, 5};
         int target = 10;
         int[] ans = twosum(nums, target);
         System.out.println(Arrays.toString(ans));
