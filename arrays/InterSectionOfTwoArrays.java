342a920aa11ee6309782ceecc1ad06a630f7c588
abhizeit <abhijeitsarraf@gmail.com>
2022-07-09 22:13:44 +0530

    solve intersection of two arrays II

---
+++ arrays/InterSectionOfTwoArrays.java
@@ -0,0 +1,44 @@
+package leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InterSectionOfTwoArrays {
+    static int[] intersect(int[] nums1, int[] nums2) {
+        ArrayList <Integer> list = new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+        int p1=0, p2=0;
+        while(p1<nums1.length && p2<nums2.length){
+            if(nums1[p1]<nums2[p2]){
+                p1++;
+            }else if(nums2[p2]<nums1[p1]){
+                p2++;
+            }else{
+                list.add(nums1[p1]);
+                p1++;
+                p2++;
+            }
+        }
+        System.out.println(list.size());
+        int[]arr = new int[list.size()];
+        int i = 0;
+        while (list.size()>i){
+            System.out.println("hell0");
+            System.out.println(list.get(i));
+            arr[i]= list.get(i);
+            i++;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[]nums1 ={4,9,5};
+        int[] nums2 ={9,4,9,8,4};
+        int[] ans = intersect(nums1,nums2);
+        System.out.println(Arrays.toString(ans));
+
+    }
+}
